0bc3b69a7b91bd5989a3fd418b6b456b955b909b
Joshua Huggins <jlhuggin@ucsc.edu>
2021-04-08 22:11:39 -0700

    Day 2 Lecture (Late) Commit

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ let config = {
     scene: [Menu, Play]
 }
 
-
 let game =  new Phaser.Game(config);
 
+//set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+let starSpeed = 4;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,11 +1,33 @@
 class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
+    }
 
+    preload(){
+        //load images or tile sprites
+        this.load.image('rocket','./assets/rocket.png');
+        this.load.image('spaceship','./assets/spaceship.png');
+        this.load.image('starfield','./assets/starfield.png');        
     }
 
     create() {
-        this.add.text(20, 20, "Rocket Patrol Play");
+        //starfield layer comes first.
+        this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0, 0);
+
+       // green UI background
+       this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+       //white borders
+       this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0); //top border
+       this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0); //bottom border
+       this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);//left border
+       this.add.rectangle(game.config.width - borderUISize,0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+
+       //add rocket (player 1)
+       this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+    }
+
+    update() {
+        this.starfield.tilePositionX -= starSpeed;
     }
 
 }
